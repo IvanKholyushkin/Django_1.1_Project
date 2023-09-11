a894b964abc3a121690b1aa1e45eaae0f6ffeca1
Ivan Kholyushkin <ivan.kholyushkin@gmail.com>
2023-09-12 02:00:50 +0300

    Third commit

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "netology_classified_ads",
-        "PASSWORD": "skvot4847233",
+        "PASSWORD": "***",
         "USER": "postgres",
         "HOST": "127.0.0.1",
         "PORT": "5432",
